19c6f6f3af06ebc7fd7eb4198f38803af948d7fe
Tophex <152851753+Tophex@users.noreply.github.com>
2024-09-30 18:57:10 -0500

    Update camara.py

--- camara.py
+++ camara.py
@@ -1,14 +1,30 @@
 import cv2
+import os
 
 # Carga el clasificador de caras
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+haarcascade_path = os.path.join(cv2.__file__, '..', 'data', 'haarcascades', 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(haarcascade_path)
+
+# Verifica si se cargó correctamente el clasificador
+if face_cascade.empty():
+    print("Error: No se pudo cargar el clasificador de caras.")
+    exit()
 
 # Inicia la captura de video desde la cámara
 cap = cv2.VideoCapture(0)
 
+# Verifica si la cámara se abrió correctamente
+if not cap.isOpened():
+    print("Error: No se pudo abrir la cámara.")
+    exit()
+
 while True:
     # Captura un fotograma
     ret, frame = cap.read()
+    if not ret:
+        print("Error: No se pudo capturar el video.")
+        break
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detecta caras
