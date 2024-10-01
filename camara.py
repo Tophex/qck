d75b91199b47547cbdc19352d8e469f9813ee21d
Tophex <152851753+Tophex@users.noreply.github.com>
2024-10-01 17:02:35 -0500

    Update camara.py

--- camara.py
+++ camara.py
@@ -1,47 +1,19 @@
-import cv2
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
 
 # Inicializa la cámara
 picam2 = Picamera2()
 
-# Configura la vista previa
-preview_config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)})
-picam2.configure(preview_config)
-
-# Inicia la vista previa
-picam2.start_preview(Preview.QT)
-
 # Inicia la cámara
 picam2.start()
 
-# Carga el clasificador de Haar para la detección de rostros
-haarcascade_path = '/home/usc/Desktop/haarcascades/haarcascade_frontalface_default.xml'
-face_cascade = cv2.CascadeClassifier(haarcascade_path)
+# Captura una imagen
+image = picam2.capture_array()
 
-try:
-    while True:
-        # Captura el frame
-        frame = picam2.capture_array()
+# Guarda la imagen
+output_path = 'foto.jpg'  # Cambia el nombre del archivo según lo necesites
+picam2.save_image(output_path, image)
 
-        # Convierte el frame a escala de grises
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+# Detiene la cámara
+picam2.stop()
 
-        # Detección de rostros
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-        # Dibuja rectángulos alrededor de los rostros detectados
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # Muestra el frame con detección de rostros
-        cv2.imshow('Detección de Rostros', frame)
-
-        # Sale del bucle si se presiona 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-except KeyboardInterrupt:
-    print("Captura Detenida.")
-finally:
-    picam2.stop()
-    cv2.destroyAllWindows()  # Cierra las ventanas de OpenCV
+print(f'Imagen guardada como {output_path}')
